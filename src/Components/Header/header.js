7010d4bc8d6805d7425b02b207dbefe8b1a57351
nitin621 <70137363+nitin621@users.noreply.github.com>
2023-05-03 16:54:41 -0700

    first commit

---
+++ src/Components/Header/header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './css/header.css'
+import logo from  '../images/logo.png'
+import InboxIcon from '@mui/icons-material/Inbox';
+import SearchIcon from '@mui/icons-material/Search';
+import { Avatar } from '@mui/material';
+const header = () => {
+  return (
+    <header>
+      <div className='header-container'>
+        <div className='header-left'>
+          <img src={logo} alt="" />
+          <h3>ICAR Discussions</h3>
+        </div>
+        <div className='header-centre'>
+          <div className='header-search-container'>
+          <SearchIcon/>
+          <input type="text" placeholder='Search message'/>
+          </div>
+        </div>
+        <div className='header-right'>
+          <div className='header-right-container'>
+           <Avatar/>
+           <InboxIcon/>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default header
