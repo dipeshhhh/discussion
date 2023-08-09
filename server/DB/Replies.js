4e09a7884c4e3ff6db3229589594804cf69607a1
CHHAVIKANT <66697124+ApeSkillx@users.noreply.github.com>
2023-08-09 14:56:24 +0530

    Adding reply backend

---
+++ server/DB/Replies.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const ReplySchema = new mongoose.Schema({
+  replied_to: [],
+  body: String,
+  auth: String,
+  replies: [{}],
+  created_at: {
+    type: Date,
+  }
+})
+
+module.exports = mongoose.model('Replies', ReplySchema)
