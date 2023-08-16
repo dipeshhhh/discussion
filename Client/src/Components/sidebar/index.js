b5903abdf02b6e5b612004581e82f3af974286b8
CHHAVIKANT <66697124+ApeSkillx@users.noreply.github.com>
2023-08-16 23:50:21 +0530

    Refactor: Decouple Sidebar for Improved Reusability
    Made sidebar independent of data from 'index.js' to improve reusability.
    Also, minor UI changes.

--- Client/src/Components/sidebar/index.js
+++ Client/src/Components/sidebar/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './css/index.css'
 import Sidebar from './sidebar'
 import Main from './main'
@@ -7,53 +7,37 @@ import Cookies from 'js-cookie';
 
 
 const Index = () => {
-
-  let [questions, setQuestions] = useState([])
-  
-  const userData = Cookies.get('auth')
-  let auth
-  if(userData)
-  {
-    const data = userData.split(',')
-    auth = data[0]  
+  // ============================================== //
+  // Suggestion: put the code below in another file 
+  // and then import it here, so other files using 
+  // this same code can import it as well. 
+  // This will improve reusability.
+  // ================ This Code ================== //
+  const userData = Cookies.get('auth');
+  let auth;
+  if (userData) {
+    const data = userData.split(',');
+    auth = data[0];
   }
+  // ============================================== //
 
-  useEffect(()=>{
-    async function getQuestion()
-    {
-      await axios.get(`/subject_question/${auth}`).then((res)=>{     
-       
-        setQuestions(res.data)
-      }).catch((err)=>{
-        console.log(err)
-      })
+  let [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    async function getQuestion() {
+      await axios.get(`/subject_question/${auth}`)
+        .then(res => setQuestions(res.data))
+        .catch(err => console.log(err));
     }
     getQuestion()
-  },[])
+  }, [])
 
-  const [group, setGroup] = useState('')
-
-  useEffect(()=>{
-    async function getGroup()
-    {
-      await axios.get(`/group/${auth}`).then((res)=>{        
-                
-          setGroup(res.data)
-        
-      }).catch((err)=>{
-        console.log(err)
-      })
-    }
-    getGroup()
-  },[])  
-  
   return (
     <div className='stack-index'>
-
-        <div className='stack-index-content'>
-            <Sidebar Subject={group}/>
-            <Main questions= {questions} />
-        </div>
+      <div className='stack-index-content'>
+        <Sidebar />
+        <Main questions={questions} />
+      </div>
     </div>
   )
 }
